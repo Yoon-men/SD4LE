60e71b68d09ba1d11aa64594bfefc312945746b6
nem-nooy <sgu101310@naver.com>
2024-05-03 22:33:59 +0900

    [EDIT] Main for v1.1.0

--- SD4LE_Main.py
+++ SD4LE_Main.py
@@ -1,9 +1,9 @@
 """
 SD4LE, Sandevistan for labsafety education
 
-ver 1.0.0
+ver 1.1.0
 
-~ Tue, Apr 30, 2024 ~
+~ Thu, May 2, 2024 ~
 """
 
 #* ------------------------------------------------------------ *#
@@ -28,6 +28,11 @@ from SD4LE_UI_Loading import LoadingUI
 from SD4LE_Logger import *
 from SD4LE_KeyFn import Sandevistan
 from SD4LE_Encryption import *
+from SD4LE_Version_Checker import version_check
+
+#* ------------------------------------------------------------ *#
+
+version = "1.1.0"
 
 #* ------------------------------------------------------------ *#
 
@@ -51,6 +56,12 @@ class Main(QObject) :
 
         mainUI.show()
         self.signal()
+
+        version_check_res = version_check(version)
+        if version_check_res: 
+            alertUI.github_ok_BT.show()
+            self.alert(version_check_res)
+
         sys.exit(app.exec_())
 
         # --- End of __init__() --- #
@@ -68,11 +79,10 @@ class Main(QObject) :
         ## Sandevistan Operating Thread
         operatingThread.show_loading_window_signal.connect(loadingUI.exec_)
         operatingThread.alert_occured_signal.connect(self.alert)
-        operatingThread.close_loading_window_signal.connect(loadingUI.close)
-
         
         # --- End of signal() --- #
-    
+
+
 
     def quit(self) -> None: 
         QOperatingThread.quit()
@@ -98,7 +108,7 @@ class Main(QObject) :
 class OperatingThread(QObject): 
     show_loading_window_signal = Signal()
     alert_occured_signal = Signal(str)
-    close_loading_window_signal = Signal()
+
 
     def operate(self) -> int: 
         logger.info("START: 산데비스탄 가동")
@@ -106,14 +116,16 @@ class OperatingThread(QObject):
 
         if (mainUI.userID_LE.text() == '') or (mainUI.userPW_LE.text() == ''): 
             self.alert_occured_signal.emit("ID, PW가 모두 입력되었는지\n확인해 주세요.")
-            logger.warning("STOP : 산데비스탄 가동 중지(사용자 계정 정보 미입력)")
+            logger.warning("STOP : 산데비스탄 가동 중지 (사용자 계정 정보 미입력)")
             return 1
-        
-        if not mainUI.userID_LE.text() in load_and_decryption().split('\n'): 
-            self.alert_occured_signal.emit("프로그램 사용 권한이 없습니다.\n개발자에게 문의하세요.")
-            logger.warning("STOP : 산데비스탄 가동 중지(사용 권한 미보유)")
+
+        user_check_res = user_check(mainUI.userID_LE.text())
+        if user_check_res: 
+            self.alert_occured_signal.emit(user_check_res)
+            logger.warning("STOP : 산데비스탄 가동 중지 (user_check)")
             return 1
-        
+
+
         chrome_options = Options()
         chrome_options.add_argument("start-maximized")
         chrome_options.add_argument(f"user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{get_chrome_version()} Safari/537.36")
@@ -126,29 +138,26 @@ class OperatingThread(QObject):
         driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
         driver.implicitly_wait(2)
 
-        sandevistan = Sandevistan(driver, logger)
+        sandevistan = Sandevistan(driver, logger, mainUI.userID_LE.text(), mainUI.userPW_LE.text())
 
-        if sandevistan.this_fffire(): 
-            logger.error("STOP : 산데비스탄 가동 중지(this_fffire)")
-            return 1
-        if sandevistan.whos_ready_for_tomorrow(mainUI.userID_LE.text(), mainUI.userPW_LE.text()): 
-            self.alert_occured_signal.emit("잘못된 계정 정보입니다.\nID와 PW를 다시 확인해 주세요.")
-            logger.warning("STOP : 산데비스탄 가동 중지(whos_ready_for_tomorrow)")
-            return 1
-        if sandevistan.friday_night_fire_fight(): 
-            logger.error("STOP : 산데비스탄 가동 중지(friday_night_fire_fight)")
-            return 1
-        if sandevistan.i_really_want_to_stay_at_your_house(): 
-            logger.error("STOP : 산데비스탄 가동 중지(i_really_want_to_stay_at_your_house)")
-            return 1
-        if sandevistan.let_you_down(): 
-            logger.error("STOP : 산데비스탄 가동 중지(let_you_down)")
-            return 1
+        cheap_but_similar = {
+            "this_fffire"                        : "STOP : 산데비스탄 가동 중지 (this_fffire)",
+            "whos_ready_for_tomorrow"            : "STOP : 산데비스탄 가동 중지 (whos_ready_for_tomorrow)",
+            "friday_night_fire_fight"            : "STOP : 산데비스탄 가동 중지 (friday_night_fire_fight)",
+            "i_really_want_to_stay_at_your_house": "STOP : 산데비스탄 가동 중지 (i_really_want_to_stay_at_your_house)",
+            "let_you_down"                       : "STOP : 산데비스탄 가동 중지 (let_you_down)"
+        }
+
+        for function, message in cheap_but_similar.items():
+            res = getattr(sandevistan, function)()
+            if res:
+                self.alert_occured_signal.emit(res)
+                logger.error(message)
+                return 1
 
 
-        self.close_loading_window_signal.emit()
-        logger.info("E N D: 산데비스탄 완료")
         self.alert_occured_signal.emit("작업을 완료했습니다.")
+        logger.info("E N D: 산데비스탄 완료")
 
         # --- End of operate() --- #
 
@@ -157,11 +166,12 @@ class OperatingThread(QObject):
 
 
 
+
 def launch() -> None : 
     global logger
     logger = init_logger(
         name="Sandevistan for labsafety education", 
-        version="1.0.0", 
+        version="1.1.0", 
         c_level=DEBUG, 
         f_level=INFO,
         f_path="./SD4LE_log"
@@ -171,9 +181,6 @@ def launch() -> None :
     Main(app)
 
     # --- End of launch() --- #
-
-
-
 
 
 if __name__ == "__main__" : 
